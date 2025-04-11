7208ad2f088c3ccd47bc5109306338b63fdfe763
BlackMin23 <delaguilalopezel@gmail,com>
2025-04-11 08:57:59 -0500

    Semana03

--- Assets/Personajes/Ninja/script/PlayerControler.cs
+++ Assets/Personajes/Ninja/script/PlayerControler.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 
@@ -6,6 +7,8 @@ public class PlayerControler : MonoBehaviour
 {
 
     Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,11 +19,36 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocityX = 0;
-        if (Input.GetKey(KeyCode.RightArrow)) {
 
-            rb.linearVelocityX = 10;
-        }
+        moverseHorizontalmente();
+        setupSalto();
         
+    }
+
+    void setupSalto()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.linearVelocityY = 20;
+        }
+    }
+
+    void moverseHorizontalmente()
+    {
+        rb.linearVelocityX = 0;
+        animator.SetInteger("Estado", 0);
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.linearVelocityX = 10;
+            sr.flipX = false;
+            animator.SetInteger("Estado", 1);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.linearVelocityX = -10;
+            sr.flipX = true;
+            animator.SetInteger("Estado", 1);
+        }
     }
 }
